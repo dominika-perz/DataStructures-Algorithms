8c46d9f68bde5b87ada5f0206c0379f5da3b7799
dominika-perz <dominika_perz@gmail.com>
2020-04-27 16:46:29 +0200

    Added largest continuous sum implementation and tests

--- Arrays/InterviewProblems.py
+++ Arrays/InterviewProblems.py
@@ -59,3 +59,39 @@ def array_pair_sum(array, result):
             pairs.add((min(a, result - a), max(a, result - a)))
 
     return len(pairs)
+
+
+# Finding the missing element
+def find_missing_sum(full, missing):
+    return sum(full) - sum(missing)
+
+
+def find_missing_xor(full, missing):
+    result = 0
+    for num in full+missing:
+        result ^= num
+    return result
+
+
+# Largest continuous sum
+def large_cont_sum(array):
+    current_start = array[0]
+    max_sum = 0
+    current_sum = 0
+    start, stop = current_start, 0
+    change = False
+    for num in array:
+        if num > current_sum + num:
+            change = True
+            current_start = num
+            current_sum = num
+        else:
+            current_sum += num
+        if current_sum > max_sum:
+            if change:
+                start = current_start
+                change = False
+            stop = num
+            max_sum = current_sum
+
+    return max_sum, start, stop
--- Arrays/test_InterviewProblems.py
+++ Arrays/test_InterviewProblems.py
@@ -1,20 +1,20 @@
 import unittest
 from unittest import TestCase
-from timeit import timeit
+import time
 from Arrays.InterviewProblems import is_anagram, is_anagram2, is_anagram3
 from Arrays.InterviewProblems import array_pair_sum
-import timeit
+from Arrays.InterviewProblems import find_missing_sum, find_missing_xor
+from Arrays.InterviewProblems import large_cont_sum
 
 
 class TestInterviewProblems(TestCase):
     def setUp(self):
-        self._started_at = timeit.timeit()
+        self._started_at = time.time()
 
     def tearDown(self):
-        elapsed = timeit.timeit() - self._started_at
+        elapsed = time.time() - self._started_at
         print(' ({:.5})'.format(elapsed), end=" ")
 
-    @unittest.skip('Anagram done')
     def test_anagram(self):
         self.assertTrue(is_anagram('dog', 'god'))
         self.assertTrue(is_anagram('clint eastwood', 'old west action'))
@@ -26,7 +26,6 @@ class TestInterviewProblems(TestCase):
         self.assertFalse(is_anagram('123', '1 2'))
         self.assertFalse(is_anagram('aa', 'bb'))
 
-    @unittest.skip('Anagram done')
     def test_anagram2(self):
         self.assertTrue(is_anagram2('dog', 'god'))
         self.assertTrue(is_anagram2('clint eastwood', 'old west action'))
@@ -38,7 +37,6 @@ class TestInterviewProblems(TestCase):
         self.assertFalse(is_anagram2('123', '1 2'))
         self.assertFalse(is_anagram2('aa', 'bb'))
 
-    @unittest.skip('Anagram done')
     def test_anagram3(self):
         self.assertTrue(is_anagram3('dog', 'god'))
         self.assertTrue(is_anagram3('clint eastwood', 'old west action'))
@@ -55,3 +53,20 @@ class TestInterviewProblems(TestCase):
         self.assertEqual(array_pair_sum([1, 9, 2, 8, 3, 7, 4, 6, 5, 5, 13, 14, 11, 13, -1], 10), 6)
         self.assertEqual(array_pair_sum([1, 2, 3, 1], 3), 1)
         self.assertEqual(array_pair_sum([1, 3, 2, 2], 4), 2)
+
+    def test_find_missing_sum(self):
+        self.assertEqual(find_missing_sum([5, 5, 7, 7], [5, 7, 7]), 5)
+        self.assertEqual(find_missing_sum([1, 2, 3, 4, 5, 6, 7], [3, 7, 2, 1, 4, 6]), 5)
+        self.assertEqual(find_missing_sum([9, 8, 7, 6, 5, 4, 3, 2, 1], [9, 8, 7, 5, 4, 3, 2, 1]), 6)
+        self.assertEqual(find_missing_sum(range(100000), range(99999)), 99999)
+
+    def test_find_missing_xor(self):
+        self.assertEqual(find_missing_xor([5, 5, 7, 7], [5, 7, 7]), 5)
+        self.assertEqual(find_missing_xor([1, 2, 3, 4, 5, 6, 7], [3, 7, 2, 1, 4, 6]), 5)
+        self.assertEqual(find_missing_xor([9, 8, 7, 6, 5, 4, 3, 2, 1], [9, 8, 7, 5, 4, 3, 2, 1]), 6)
+        self.assertEqual(find_missing_sum(range(100000), range(99999)), 99999)
+
+    def test_large_cont_sum(self):
+        self.assertEqual(large_cont_sum([1, 2, -1, 3, 4, -1]), (9, 1, 4))
+        self.assertEqual(large_cont_sum([1, 2, -1, 3, 4, 10, 10, -10, -1]), (29, 1, 10))
+        self.assertEqual(large_cont_sum([-1, 1]), (1, 1, 1))
