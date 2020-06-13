547d02fedfd4aa5410b1234a2920d4e9bf07354e
dominika-perz <dominika_perz@gmail.com>
2020-06-13 19:43:58 +0200

    Refactor of module names

--- data_structures/linked_lists/test_LinkedLists.py
+++ data_structures/linked_lists/test_LinkedLists.py
@@ -4,49 +4,37 @@ from data_structures.linked_lists.DoubleLinkedListNode import *
 
 
 class TestInterviewProblems(unittest.TestCase):
-    def test_linked_list_reversal(self):
-        # Create a list of 4 nodes
-        head = SingleLinkedListNode(1)
+    def setUp(self):
+        self.head = SingleLinkedListNode(1)
         b = SingleLinkedListNode(2)
-        head.next = b
+        self.head.next = b
         c = SingleLinkedListNode(3)
         b.next = c
         d = SingleLinkedListNode(4)
+        self.middle = d
         c.next = d
+        e = SingleLinkedListNode(5)
+        d.next = e
 
-        reversed_head = linked_list_reversal(head)
+    def tearDown(self):
+        pass
+
+    def test_linked_list_reversal(self):
+        reversed_head = linked_list_reversal(self.head)
         node = reversed_head
-        for i in range(4, 0, -1):
+        for i in range(5, 0, -1):
             self.assertEqual(node.data, i)
             if node.next is not None:
                 node = node.next
 
     def test_nth_to_last(self):
-        head = SingleLinkedListNode(1)
-        b = SingleLinkedListNode(2)
-        head.next = b
-        c = SingleLinkedListNode(3)
-        b.next = c
-        d = SingleLinkedListNode(4)
-        c.next = d
-        e = SingleLinkedListNode(5)
-        d.next = e
-        self.assertEqual(nth_to_last(2, head), d)
+        self.assertEqual(nth_to_last(2, self.head), self.middle)
 
     def test_cycle_check(self):
-        head = SingleLinkedListNode(1)
-        b = SingleLinkedListNode(2)
-        head.next = b
-        c = SingleLinkedListNode(3)
-        b.next = c
-        d = SingleLinkedListNode(4)
-        c.next = d
-        e = SingleLinkedListNode(5)
-        d.next = e
-        self.assertFalse(cycle_check(head))
+        self.assertFalse(cycle_check(self.head))
 
-        e.next = c
-        self.assertTrue(cycle_check(head))
+        self.middle.next = self.head
+        self.assertTrue(cycle_check(self.head))
 
 
 if __name__ == '__main__':
